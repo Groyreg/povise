a66a20a80029f72a0ee7bdb3d7cf4113e68ad301
Кочарян Григорий <groyreg@gmail.com>
2022-12-10 00:30:54 +0300

    add eslint add styled components add theme add switched for theme add aliases

---
+++ .eslintrc.js
@@ -0,0 +1,125 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:typescript-sort-keys/recommended',
+  ],
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    react: {
+      version: 'detect',
+    },
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'simple-import-sort', 'sort-keys-fix', 'typescript-sort-keys', 'lint-local'],
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'error', // return function type
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/extensions': [
+      // initial config
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'lint-local/custom-max-lines-per-function': [
+      'error',
+      {
+        functionsMaxLines: 35,
+        reactComponentMaxLines: 200,
+        reactHooksCallbackMaxLines: 1,
+      },
+    ],
+
+    'lint-local/group-hooks': ['error'],
+    'lint-local/hooks-typings': 'error',
+    'lint-local/no-inline-callbacks': 'error',
+    'lint-local/no-literal-types': 'error',
+    'lint-local/no-statement-inside-statements': 'error',
+    'lint-local/sort-methods': 'error',
+    'lint-local/spacing-control': 'error',
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-useless-return': 'error',
+    'object-curly-newline': 'off',
+    'prefer-destructuring': [
+      'error',
+      {
+        AssignmentExpression: {
+          array: true,
+          object: true,
+        },
+        VariableDeclarator: {
+          array: true,
+          object: true,
+        },
+      },
+      {
+        enforceForRenamedProperties: true,
+      },
+    ],
+    'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.ts', '.js', '.jsx'] }], // initial config
+    'react/jsx-one-expression-per-line': 'off', // conflict with prettier
+    'react/react-in-jsx-scope': 'off', // initial config
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-keys': ['error', 'asc', { caseSensitive: true, minKeys: 2, natural: true }],
+    'sort-keys-fix/sort-keys-fix': 'error',
+    'typescript-sort-keys/interface': 'error',
+    'typescript-sort-keys/string-enum': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  parser: 'typescript',
+  semi: true,
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        parser: 'html',
+      },
+    },
+    {
+      files: ['*.js', '*.cjs', '*.mjs'],
+      options: {
+        parser: 'flow',
+      },
+    },
+
+    {
+      files: '*.ts',
+      options: {
+        parser: 'typescript',
+      },
+    },
+
+    {
+      files: ['*.json', '*.jsonc', '.babelrc', '.prettierrc', '.eslintrc'],
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+};
